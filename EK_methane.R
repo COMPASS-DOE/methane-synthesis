92703ea30f8a815ae209c7f3b24486a02aff1c38
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2022-02-08 13:16:45 -0500

    add Manipulation

--- EK_methane.R
+++ EK_methane.R
@@ -1,11 +1,8 @@
 
-#methane dataset gathered by Emily Kang
-getwd()
-#setwd() is not a coding best practice
-#simply save files within the same folder
-#use as many instances of R studio as needed to maintain distinct directories
+library(dplyr)
+library(ggplot2)
 
-setwd("C:/Users/morr497/OneDrive - PNNL/Documents/methane/")
+#methane dataset gathered by Emily Kang
 EK_meth <- read.csv("EK_methane.csv")
 
 #need to convert all values to common units of mg CH4 m2 d
@@ -26,5 +23,15 @@ mutate(cf = con_fac[Unit],
        CH4_manip = CH4_manip * cf,
        SD_CH4_manip =  SD_CH4_manip * cf,
        SE_CH4_manip = SE_CH4_manip * cf,
-       Unit = "mg CH4 m2 d") -> EK_meth
+       Unit = "mg CH4 m2 d",
+  Manipulation = case_when(
+    Percent_control < 100 ~ "Drought",
+    Percent_control > 100 ~ "Irrigation",
+    TRUE ~ NA_character_))-> EK_meth
 
+
+ggplot(EK_meth, 
+       aes(Percent_control, CH4_manip - CH4_control)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(. ~ Manipulation, scales = "free")
